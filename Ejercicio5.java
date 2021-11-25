72c498e90a767600387b73bbf8967b4c8f4e34fa
Lidia Zap <tiristar2015@gmail.com>
2021-11-25 03:21:08 -0300

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -8,12 +8,11 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         List<Alumnos> alumnos = List.of(
                 new Alumnos("Homero", "Simpson", LocalDate.now().minusYears(30)),
-                new Alumnos("Lisa", "Simpson", LocalDate.of(1956, 5, 12)),
-                new Alumnos("Bart", "Simpson", LocalDate.of(1990, 2, 10)),
-                new Alumnos("Maggie", "Simpson", LocalDate.of(2001, 3, 30)),
-                new Alumnos("Marge", "Bouvier", LocalDate.of(1954, 2, 5)),
-                new Alumnos("Marvin", "Monroe", LocalDate.of(1945, 7, 21)),
-                new Alumnos("Abbe", "Simpson", LocalDate.of(1911, 11, 11)));
+                new Alumnos("Maria", "Perez", LocalDate.of(1956, 5, 12)),
+                new Alumnos("Julio", "Smith", LocalDate.of(1990, 2, 10)),
+                new Alumnos("Elena", "Barrios", LocalDate.of(2001, 3, 30)),
+                new Alumnos("Pedro", "Lobo", LocalDate.of(1954, 2, 5)),
+                new Alumnos("Juan", "Martinez", LocalDate.of(1911, 11, 11)));
         Map<String, Integer> alumnosConEdades = alumnos.stream()
                 .collect(Collectors
                         .toMap(p-> (p.getName() + " " + p.getSurname()),
